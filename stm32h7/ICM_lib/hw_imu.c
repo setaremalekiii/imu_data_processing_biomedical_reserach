a3bd9ef546d5c5382bb74c5b6cee12cbbd911fdb
Setare Maleki <setimalek@gmail.com>
2026-02-09 14:04:19 -0800

    small edits to the lib to wake up imu

--- stm32h7/ICM_lib/hw_imu.c
+++ stm32h7/ICM_lib/hw_imu.c
@@ -9,103 +9,84 @@ static void deactivate_imu(){
 }
 
 static void sel_user_bank(user_bank_t bank){
-    uint8_t tx[2] = { (uint8_t)(REG_BANK_SEL & 0x7F), (uint8_t)((bank&0x3)<<4) }; // write 0x7F then bank
-    //uint8_t rx[2] = {0};
+    uint8_t tx[2] = { (uint8_t)(REG_BANK_SEL & 0x7F), (uint8_t)(bank) }; // write 0x7F then bank
+    // adding rx call for testing 
+    uint8_t rx[2] = {0};
     activate_imu();
     // HAL_StatusTypeDef spi_status;
     HAL_SPI_Transmit(&hspi1, tx, 2, 100);
-    // HAL_SPI_Receive(&hspi1, rx, 2, 100);
+    HAL_SPI_Receive(&hspi1, rx, 2, 100);
     deactivate_imu();
     uint8_t who;
     uint8_t who_am_i = rx[1];
-    // spi_status  = HAL_SPI_Transmit(&hspi1, &data,   1, 100);
 }
 
 
 void imu_init(void){
     HAL_GPIO_WritePin(GPIOA, GPIO_PIN_4, GPIO_PIN_SET);
     uint8_t temp_data;
-    // imu_write_reg(_b0,PWR_MGMT_1, 0xc1);
-    imu_write_reg(_b0, PWR_MGMT_1, 0x01);
-    imu_write_reg(_b2,ODR_ALIGN_EN, 0x01);
-    imu_write_reg(_b2,  ACCEL_SIMLRT_DIV_1,0x00);
+    imu_write_reg(_b0,PWR_MGMT_1, 0xc1); //resets imu sensor
+    HAL_Delay(100); // givnig some time for imu to wake up and be ready for communication
+    imu_write_reg(_b0, PWR_MGMT_1, 0x01); // exit sleep mode 
+    imu_write_reg(_b2,ODR_ALIGN_EN, 0x01); // data alignment -> synchornization not rlly needed 
+    imu_write_reg(_b2,  ACCEL_SIMLRT_DIV_1,0x00); // setting sampling rate this is the divider =0
     imu_write_reg(_b2,  ACCEL_SMPLRT_DIV_2,0x00);
-    imu_write_reg(_b2, ACCEL_CONFIG, ((ACCEL_RANGE_VALUE<< 1)|0x01));    
-    imu_read_reg(_b0,USER_CTRL, &temp_data);
+    imu_write_reg(_b2, ACCEL_CONFIG, ((ACCEL_RANGE_VALUE<< 1)|0x01)); // turning on the digital lowpass filter
+    imu_read_reg(_b0, USER_CTRL, &temp_data); // serial interface entering spi mode
     temp_data |= 0x10;
-    imu_write_reg(_b0, USER_CTRL,temp_data);    
+    imu_write_reg(_b2, USER_CTRL, temp_data); 
     sel_user_bank(_b0);
 }
 
 void imu_write_reg(user_bank_t bank, uint8_t reg, uint8_t data){
     sel_user_bank(bank);
-    HAL_Delay(1); // give it a moment
-
-    // uint8_t tx[2] = {reg, data};
-    // activate_imu();
-    // HAL_StatusTypeDef spi_status;
-    // spi_status = HAL_SPI_Transmit(&hspi1, tx, 2, 100);
-    // deactivate_imu();
-    //spi_status = HAL_SPI_Transmit(&hspi1, &data, 1, 100);
-        //sel_user_bank(bank);
-    uint8_t tx[2] = { (uint8_t)(reg & 0x7F), data }; // MSB=0 for write
+    uint8_t tx[2] = {reg, data};
     activate_imu();
     HAL_SPI_Transmit(&hspi1, tx, 2, 100);
     deactivate_imu();
-
 }
 
 void imu_read_reg(user_bank_t bank, uint8_t address, uint8_t *data){
     sel_user_bank(bank);
-    //uint8_t temp_data = 0x80 | address; // MSB 1 for read
-    uint8_t tx[2] = { (uint8_t)(0x80 | (address & 0x7F)), 0x2F };
-    uint8_t rx[2];
+    uint8_t temp_data = 0x80 | address; // MSB 1 for read
     activate_imu();
     HAL_StatusTypeDef spi_status;
-    spi_status = HAL_SPI_TransmitReceive(&hspi1, tx, rx, 2, 100);
+    spi_status = HAL_SPI_Transmit(&hspi1, &temp_data, 1, 100);
+    // watch data variable to see if the correct value is read
+    HAL_SPI_Recieve(&hspi1, data, 1 , 100);
     deactivate_imu();
-    *data = rx[1];
 }
 
 
 void imu_read_data(imu_data_t *data){
-//   uint8_t tx2[2] = { 0x7f , 0x00};  // WHO_AM_I (0x00) + READ bit, dummy
-//   uint8_t rx2[2] = { 0 };
-
-//   activate_imu();
-//   HAL_SPI_TransmitReceive(&hspi1, tx2, rx2, 2, 1000);
-//   deactivate_imu();
-
-//   uint8_t who_am_i = rx2[1];   // THIS is the WHO_AM_I value
-//     HAL_Delay(1000);
-    // uint8_t data_rx[12];
-    // uint8_t temp_data = 0x80|ACCEL_XOUT_H;
-    // activate_imu();
-    // HAL_SPI_Transmit(&hspi1, &temp_data, 1, 100);
-    // HAL_SPI_Receive(&hspi1, data_rx, 12, 100);
-    // data -> x_accel = ((uint16_t)data_rx[0] << 8) | data_rx[1];
-    // data -> y_accel = ((uint16_t)data_rx[2] << 8) | data_rx[3];
-    // data -> z_accel = ((uint16_t)data_rx[4] << 8) | data_rx[5];
-    // deactivate_imu();
-    // sel_user_bank(_b0);
-    HAL_Delay(1);
-    // uint8_t tx[7] = { (uint8_t)(0x80 | ACCEL_XOUT_H), 0,0,0,0,0,0 };
-    uint8_t buf[] = "hello";
-    uint8_t rx[7];
-
+    uint8_t data_rx[6];
+    uint8_t temp_data = 0x80|ACCEL_XOUT_H;
     activate_imu();
-    HAL_StatusTypeDef tatu;
-    tatu = HAL_SPI_Transmit(&hspi1, buf, sizeof(buf)/ sizeof(buf[0]), 250);
-    // tatu = HAL_SPI_TransmitReceive(&hspi1, tx, rx, sizeof(tx), 100);
+    HAL_SPI_Transmit(&hspi1, &temp_data, 1, 100);
+    HAL_SPI_Receive(&hspi1, data_rx, 12, 100);
+    // You must combine two 8-bit reads into a 16-bit signed value:
+    data -> x_accel = ((uint16_t)data_rx[0] << 8) + data_rx[1];
+    data -> y_accel = ((uint16_t)data_rx[2] << 8) + data_rx[3];
+    data -> z_accel = ((uint16_t)data_rx[4] << 8) + data_rx[5];
     deactivate_imu();
+    //trying to see if I can print the data 
+    const uint16_t buffer[] = data.x_accel;
+    HAL_UART_Transmit(&huart3, buffer, sizeof(buffer)/sizeof(buffer[0]), 100);
 
-    // rx[0] is dummy (during address phase). Data starts at rx[1].
-    data->x_accel = (int16_t)((rx[1] << 8) | rx[2]);
-    data->y_accel = (int16_t)((rx[3] << 8) | rx[4]);
-    data->z_accel = (int16_t)((rx[5] << 8) | rx[6]);
+    // sel_user_bank(_b0);
+    // uint8_t tx[7] = { (uint8_t)(0x80 | ACCEL_XOUT_H), 0,0,0,0,0,0 };
+    // uint8_t buf[] = "hello";
+    // uint8_t rx[7];
 
+    // activate_imu();
+    // HAL_StatusTypeDef tatu;
+    // tatu = HAL_SPI_Transmit(&hspi1, buf, sizeof(buf)/ sizeof(buf[0]), 250);
+    // // tatu = HAL_SPI_TransmitReceive(&hspi1, tx, rx, sizeof(tx), 100);
+    // deactivate_imu();
+
+    // // rx[0] is dummy (during address phase). Data starts at rx[1].
+    // data->x_accel = (int16_t)((rx[1] << 8) | rx[2]);
+    // data->y_accel = (int16_t)((rx[3] << 8) | rx[4]);
+    // data->z_accel = (int16_t)((rx[5] << 8) | rx[6]);
 }
 
-// You must combine two 8-bit reads into a 16-bit signed value:
-
-// int16_t raw = (high_byte << 8) | low_byte;
