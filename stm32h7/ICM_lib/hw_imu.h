b934f1d26cb94956a93a084645b1a7e21c43b13c
Setare Maleki <setimalek@gmail.com>
2026-02-09 13:58:40 -0800

    changing header

--- stm32h7/ICM_lib/hw_imu.h
+++ stm32h7/ICM_lib/hw_imu.h
@@ -4,7 +4,7 @@
 #include <stdint.h>
 #include <main.h>
 #include "stdio.h"
-#include "stm32h7xx_hal_gpio.h"
+
 
 typedef struct{
     int16_t x_accel; 
@@ -12,19 +12,16 @@ typedef struct{
     int16_t z_accel;   
 } imu_data_t;
 
-extern SPI_HandleTypeDef hspi1; 
-#define IMU_SPI hspi1
+extern SPI_HandleTypeDef hspi1;
 #define IMU_CS_PORT GPIOA
 #define IMU_CS_PIN  GPIO_PIN_4
 #define ACCEL_RANGE_VALUE _accel_2g
 
-#define REG_BANK_SEL 0x7f
+#define REG_BANK_SEL 0x7F
 #define ODR_ALIGN_EN 0x09
-#define WHO_AM_I 0x00
+#define WHO_AM_I 0
 #define PWR_MGMT_1 0x06
 #define USER_CTRL 0x03
-#define LP_CONFIG 0x05 
-#define PWR_MGMT_2 0x07
 #define ACCEL_SIMLRT_DIV_1 0x10
 #define ACCEL_SMPLRT_DIV_2 0x10
 #define ACCEL_CONFIG 0x14 
