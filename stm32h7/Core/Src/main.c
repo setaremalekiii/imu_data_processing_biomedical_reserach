26f25fdabf3f397b937c98eec8b81591742435eb
Setare Maleki <setimalek@gmail.com>
2026-02-13 13:00:11 -0800

    keep GPIO high while resetting the sensor

--- stm32h7/Core/Src/main.c
+++ stm32h7/Core/Src/main.c
@@ -356,7 +356,8 @@ static void MX_GPIO_Init(void)
   __HAL_RCC_GPIOB_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_4, GPIO_PIN_RESET);
+  // Making sure the GPIO is set high for the imu reset
+  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_4, GPIO_PIN_SET);
 
   /*Configure GPIO pin : PA4 */
   GPIO_InitStruct.Pin = GPIO_PIN_4;
